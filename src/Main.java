057aa7c0d1384b07a1e1eb39c1348ec435109e1c
Rajasekaran <proffraja@gmail.com>
2018-05-30 12:24:40 +0530

    Changes in Project Structure

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 import com.raja.oopslab.converters.Currency;
 import com.raja.oopslab.converters.Distance;
 import com.raja.oopslab.converters.Time;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println(Time.convertSecondsToHours(1));
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+        
+        while(choice != 4){
+        	System.out.println("Converters");
+        	System.out.println("**********");
+        	System.out.println("1. Currentcy\n2. Distance\n3. Time\n4. Exit\n\nEnter Your Choice");
+        	choice = input.nextInt();
+        	switch(choice){
+        	case 1:
+        		Currency.userChoice();
+        		break;
+        	case 2:
+        		convertDistance();
+        		break;
+        	case 3:
+        		convertTime();
+        		break;
+        	}
+        }
+        System.out.println("Thank You !!!!");
+    }
+    
+    public static void convertDistance(){
+    	
+    }
+    public static void convertTime() {
+    	
     }
 }
--- src/com/raja/oopslab/converters/Currency.java
+++ src/com/raja/oopslab/converters/Currency.java
@@ -1,5 +1,7 @@
 package com.raja.oopslab.converters;
 
+import java.util.Scanner;
+
 public class Currency {
 
 	public static double covertEUROtoINR(double EURO) {
@@ -25,4 +27,35 @@ public class Currency {
 	public static double convertINRtoYEN(double YEN) {
 		return YEN * 1.63;
 	}
+	
+	public static void userChoice(){
+    	Scanner input = new Scanner(System.in);
+        int currency_choice = 0;
+        double money = 0;
+        while(currency_choice != 7){
+        	System.out.println("\nCurrency Converter");
+        	System.out.println("------------------");
+        	System.out.println("1. DOLLOR to INR\n2. EURO to INR\n3. YEN to INR\n"
+        					 + "4. INR to DOLLOR\n5. INR to EURO\n6. INR to YEN\n"
+        					 + "7.Exit\n\nEnter Your Choice");
+        	currency_choice = input.nextInt();
+        	switch(currency_choice){
+        	case 1:
+        		System.out.println("Enter in DOLLERS");
+        		money = input.nextDouble();
+        		System.out.println(money+" DOLLERS is equal to "+Currency.convertDOLLARtoINR(money)+" INDIAN RUPEES");
+        		break;
+        	case 2:
+        		System.out.println("Enter in EURO");
+        		money = input.nextDouble();
+        		System.out.println(money+" EURO is equal to "+Currency.covertEUROtoINR(money)+" INDIAN RUPEES");
+        		break;
+        	case 3:
+        		System.out.println("Enter in YEN");
+        		money = input.nextDouble();
+        		System.out.println(money+" YEN is equal to "+Currency.convertYENtoINR(money)+" INDIAN RUPEES");
+        		break;
+        	}
+        }
+    }
 }
